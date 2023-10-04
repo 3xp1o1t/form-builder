3135cf6a114fc82f701e54671f747e1391610121
3xp1o1t <jes.monti.503@gmail.com>
2023-10-04 14:01:32 -0600

    Added: basic setup
    Added theme switcher dark/white mode
    Added clerk authentication
    Added navbar with logo/theme and user avatar.
    Added custom style for next-themes
    Added all shadcn components

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import Logo from '@/components/logo';
+import ThemeSwitcher from '@/components/theme-switcher';
+import { UserButton } from '@clerk/nextjs';
+import { ReactNode } from 'react';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between items-center border-b h-[60px] px-4 py-2">
+        <Logo />
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/logo.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function Logo() {
+  return (
+    <Link
+      href={'/'}
+      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer"
+    >
+      PageForm
+    </Link>
+  );
+}
+
+export default Logo;
