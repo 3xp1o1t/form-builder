391ee6da10b453c5ca8e51e51d876ace192277b2
3xp1o1t <jes.monti.503@gmail.com>
2023-10-09 15:09:04 -0600

    Added: Form preview
    Added a preview for the form created, also remove readonly and disable
    in the form preview for testing

--- components/preview-dialog-btn.tsx
+++ components/preview-dialog-btn.tsx
@@ -1,12 +1,40 @@
 import { MdPreview } from 'react-icons/md';
+import { FormElements } from './form-elements';
+import useDesigner from './hooks/use-designer';
 import { Button } from './ui/button';
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 
 function PreviewDialogBtn() {
+  const { elements } = useDesigner();
   return (
-    <Button variant={'outline'} className="gap-2">
-      <MdPreview className="h-6 w-6" />
-      Preview
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={'outline'} className="gap-2">
+          <MdPreview className="h-6 w-6" />
+          Preview
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
+        <div className="px-4 py-2 border-b">
+          <p className="text-lg font-bold text-muted-foreground">
+            Form Preview
+          </p>
+          <p className="text-sm text-muted-foreground">
+            This is how your form will look like to your users.
+          </p>
+        </div>
+        <div className="bg-accent flex flex-col flex-grow items-center justify-center p-4 bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)] overflow-y-auto">
+          <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background h-full w-full rounded-2xl p-8 overflow-y-auto">
+            {elements.map((element) => {
+              const FormComponent = FormElements[element.type].formComponent;
+              return (
+                <FormComponent key={element.id} elementInstance={element} />
+              );
+            })}
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
 
