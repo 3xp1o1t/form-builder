572b5a8dfdce38d6abf68d29dc82854e3f0629a7
3xp1o1t <jes.monti.503@gmail.com>
2023-10-10 18:08:59 -0600

    Added: title field
    Added a title field for heading 1 titles.

--- components/form-elements-sidebar.tsx
+++ components/form-elements-sidebar.tsx
@@ -1,11 +1,19 @@
 import { FormElements } from './form-elements';
 import SidebarBtnElement from './sidebar-btn-element';
+import { Separator } from './ui/separator';
 
 function FormElementsSidebar() {
   return (
     <div>
-      Elements
-      <SidebarBtnElement formElement={FormElements.TextField} />
+      <p className="text-sm text-foreground/70">Drag and drop elements</p>
+      <Separator className="my-2" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 place-items-center">
+        <p className="text-sm text-muted-foreground col-span-1 md:col-span-2 my-2 place-self-start">
+          Layout elements
+        </p>
+        <SidebarBtnElement formElement={FormElements.TextField} />
+        <SidebarBtnElement formElement={FormElements.TitleField} />
+      </div>
     </div>
   );
 }
--- components/form-elements.tsx
+++ components/form-elements.tsx
@@ -1,6 +1,7 @@
 import { TextFieldFormElement } from './fields/text-field';
+import { TitleFieldFormElement } from './fields/title-field';
 
-export type ElementsType = 'TextField';
+export type ElementsType = 'TextField' | 'TitleField';
 export type SubmitFunction = (key: string, value: string) => void;
 
 export type FormElement = {
@@ -40,4 +41,5 @@ type FormElementsType = {
 
 export const FormElements: FormElementsType = {
   TextField: TextFieldFormElement,
+  TitleField: TitleFieldFormElement,
 };
